d5ea404732716837ef3af650708d8ec32a697cf5
Waldir Borba Junior <wborbajr@gmail.com>
2020-11-21 13:22:49 -0300

    path fix

--- NFeRun.sh
+++ NFeRun.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # while :; do python /nfeimport/NFeDownloadXML.py; sleep 60; done &
 # while :; do python /nfeimport/NFeImport.py xml/; sleep 120; done &
 
 # Download dos XMLs do email
-watch -n 60 python /nfeimport/NFeDownloadXML.py
+/opt/venv/bin/python NFeDownloadXML.py
 
 # Carga dos XMLs
-watch -n 120 python /nfeimport/NFeImport.py xml/
+/opt/venv/bin/python NFeImport.py xml/
